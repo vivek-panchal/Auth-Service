aa8302d84f505d06949c0dfc3e7c1879e20f8179
vivek-panchal <vivek.nsut@gmail.com>
2025-12-06 19:19:22 +0530

    implement email verification process with OTP and token-based verification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,12 @@
 import TryCatch from "../middlewares/TryCatch.js";
 import sanitize from "mongo-sanitize";
+import { redisClient } from "../index.js";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import sendMail from "../config/sendMail.js";
 import { registerSchema } from "../config/zod.js";
+import { getVerifyEmailHtml } from "../config/html.js";
 
 export const registerUser = TryCatch(async (req, res) => {
     // Registration logic here
@@ -24,9 +30,78 @@ export const registerUser = TryCatch(async (req, res) => {
 
     const { name, email, password } = validatedData.data;
 
-    res.json({
+    const rateLimitKey = `register-rate-limit:${req.ip}:${email}`;
+    //console.log('Checking rate limit for', req.ip, email);
+
+    if(await redisClient.get(rateLimitKey)) {
+        //console.log('Rate limit exceeded for', req.ip, email);
+        return res.status(429).json({ message: 'Too many registration attempts. Please try again later.' });
+    }
+
+    const existingUser = await User.findOne({ email }); 
+    //console.log('Existing user check for', email, existingUser);
+
+    if(existingUser) {
+        //console.log('User already exists for', email, existingUser);
+        return res.status(400).json({ message: 'User with this email already exists' });
+    }
+
+    
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const verificationToken = crypto.randomBytes(32).toString('hex');
+    const verificationKey = `verify:${verificationToken}`;
+
+    const datatoStore = {
         name,
         email,
-        password,
+        password: hashedPassword,
+    };
+    // Store as string in Redis to avoid type errors
+    // Token valid for 30 seconds
+    await redisClient.set(verificationKey, JSON.stringify(datatoStore), { EX: 300 });
+
+    const subject = 'Verify your email for Account creation';
+    const html = getVerifyEmailHtml({email, token: verificationToken});
+    //console.log('Sending verification email to', email);
+
+    await sendMail({ email, subject, html });
+    // Rate limit registration attempts per IP+email for 1 minute
+    await redisClient.set(rateLimitKey, 'true', { EX: 60 });
+
+
+    res.json({
+        message: 'Registration successful. Please check your email to verify your account. The verification link is valid for 5 minutes.',
     });
+});
+
+export const verifyUser = TryCatch(async (req, res) => {
+    const { token } = req.params;
+    console.log('Verifying user with token:', token);
+    if(!token) {
+        return res.status(400).json({ message: 'Verification token is required' });
+    }
+    const verificationKey = `verify:${token}`;
+    //console.log('Looking up verification key in Redis:', verificationKey);
+    const userDataJSON = await redisClient.get(verificationKey);
+    //console.log('Fetched user data from Redis for token:', token, 'Data:', userDataJSON);
+    // console.log('Verification attempt with token', token, 'found data:', userDataJSON);
+    if(!userDataJSON) {
+        return res.status(400).json({ message: 'Verification token or link is invalid or has expired' });
+    }
+
+    const userData = JSON.parse(userDataJSON);    
+    const { name, email, password } = userData;
+    const existingUser = await User.findOne({ email }); 
+    //console.log('Existing user check during verification for', email, existingUser);
+
+    if(existingUser) {
+        return res.status(400).json({ message: 'User with this email already exists' });
+    }
+
+    const newUser = await User.create({ name, email, password });
+
+    await redisClient.del(verificationKey);
+    res.status(201).json({ message: 'Email verified successfully. You can now log in.' , user:{_id: newUser._id, name: newUser.name, email: newUser.email } });
 });
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { createClient } from 'redis';
 
 dotenv.config();
 await connectDB();
+
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  console.log('REDIS_URL is not defined in environment variables');
+  process.exit(1);
+}
+
+export const redisClient = createClient({ url: redisUrl });
+redisClient.connect().then(() => {
+  console.log('Connected to Redis');
+}).catch((err) => {
+  console.error('Could not connect to Redis', err);
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
